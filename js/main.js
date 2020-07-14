83ea36d36c20b956a4eec79723de5fd905181d0d
akr007 <antalkrisztian0@gmail.com>
2020-07-14 18:28:58 +0200

    major & minor changes

--- js/main.js
+++ js/main.js
@@ -112,25 +112,6 @@ $(document).ready(function () {
 
 
 
-    $(".testimonial-slider").owlCarousel({
-        loop:true,
-        margin:30,
-        nav:false,
-        dots:true,
-        autoplay:true,
-        autoplayHoverPause:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
-            }
-        }
-    });
 
 	
 	
